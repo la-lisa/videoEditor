7c1cd2a6a1110288adc16f5a2b395509ea787152
Lisa Lamplmair <lisa.lamplmair@gmx.at>
2022-04-13 11:49:53 +0200

    Add ffmpeg and nodemon

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const ffmpeg = require('@ffmpeg-installer/ffmpeg');
+
 
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from Express!" });
+  res.json({ message:ffmpeg.path + ", " + ffmpeg.version
+  });
 });
 
 app.listen(PORT, () => {
